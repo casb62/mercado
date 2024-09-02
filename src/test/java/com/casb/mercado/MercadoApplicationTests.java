5e4c7a72ace5ef44d2c6a0be2fc6c9009cdab752
casb62 <casb62@gmail.com>
2024-09-02 09:40:15 -0300

    Projeto criado

---
+++ src/test/java/com/casb/mercado/MercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casb.mercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
